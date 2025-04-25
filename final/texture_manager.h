7570bae7ade4476574392036a5992c2b4f2ae833
Lquan925 <24021604@vnu.edu.vn>
2025-04-25 20:14:33 +0700

    Chem-quai

---
+++ final/texture_manager.h
@@ -0,0 +1,12 @@
+#ifndef TEXTURE_MANAGER_H
+#define TEXTURE_MANAGER_H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <string>
+
+SDL_Texture* LoadTexture(const std::string& path, SDL_Renderer* renderer);
+Mix_Chunk* LoadSound(const char* path);
+
+#endif
